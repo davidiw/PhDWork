ff4df72e7d03a03c39dd34f1bfe23d80acce9252
David Wolinsky <davidiw@ufl.edu>
2009-12-27 20:37:35 -0500

    Initial commit of proposal

---
+++ make.sh
@@ -0,0 +1,6 @@
+latex ufsampleETD -interaction=nonstopmode
+bibtex ufsampleETD
+latex ufsampleETD -interaction=nonstopmode
+latex ufsampleETD -interaction=nonstopmode
+dvipdfm ufsampleETD.dvi
+xpdf ufsampleETD.pdf
